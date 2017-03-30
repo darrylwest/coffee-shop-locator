10b337065560d5b33132cd0aa3788d311216e06c
darryl west <darryl.west@RainCitySoftware.com>
2017-03-30 12:52:02 -0700

    sandbox testing for google map api -- address to lat/lng

---
+++ tools/lat-long-from-address.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+// dpw@seattle.local
+// 2017.03.30
+'use strict';
+
+const https = require('https');
+const address='535 Mission St, San Francisco, CA 94105';
+
+const opts = {
+    method: 'GET',
+    port: 443,
+    hostname: 'maps.googleapis.com',
+    path: `/maps/api/geocode/json?address=${escape(address)}`
+};
+
+const chunks = [];
+const errors = [];
+const req = https.request(opts, (res) => {
+    // console.log('status code:', res.statusCode);
+    // console.log('headers:', res.headers);
+
+    res.on('data', (data) => {
+        chunks.push(data.toString());
+        // console.log(data);
+    });
+});
+
+
+req.on('error', (err) => {
+    console.log(err);
+    errors.push(err);
+});
+
+req.on('close', () => {
+    console.log('conn closed...');
+
+    const message = chunks.join('');
+
+    const obj = JSON.parse(message);
+    if (obj.status === 'OK') {
+        const result = obj.results.pop();
+        console.log(result.formatted_address);
+        console.log(result.geometry.location);
+    } else {
+        console.log(message);
+    }
+
+});
+
+req.end();
+
--- tools/lat-long-from-address.sh
+++ tools/lat-long-from-address.sh
@@ -3,7 +3,10 @@
 # 2017.03.30
 #
 
-address='2634+woolsey+street,+berkeley+ca'
+# address='2634+woolsey+street,+berkeley+ca'
+# address='535+Mission+St,+San+Francisco,+CA+94105'
+# js escaped address...
+address='535%20Mission%20St%2C%20San%20Francisco%2C%20CA%2094105'
 
 curl "https://maps.googleapis.com/maps/api/geocode/json?address=${address}"
 
