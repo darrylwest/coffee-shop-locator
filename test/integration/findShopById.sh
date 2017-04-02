0f70c1663510c20c46e51df72c048c85b811f317
darryl west <darryl.west@RainCitySoftware.com>
2017-04-02 10:01:10 -0700

    added findShopById good/bad news integration test

--- test/integration/findShopById.sh
+++ test/integration/findShopById.sh
@@ -7,5 +7,9 @@ host=http://localhost:3002
 apikey=01BCGE4TN75GJKR2ZS2PPGCZZH
 id=10
 
-curl -H "x-api-key: ${apikey}" -X GET "${host}/coffeeshop/${id}"
+echo "Should find shop with id ${id}"
+curl -H "x-api-key: ${apikey}" -X GET "${host}/coffeeshop/${id}" && echo ""
 
+id=999
+echo "Should not find shop with id ${id}"
+curl -H "x-api-key: ${apikey}" -X GET "${host}/coffeeshop/${id}" && echo ""
